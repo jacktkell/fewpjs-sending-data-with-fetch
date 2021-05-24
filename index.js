8aedacb13e230ddaf74a203a5a01d589dde1dd80
jack <jack.kelling7@gmail.com>
2021-05-24 15:52:09 -0500

    commit tree e65867d0ac986f1a54e4b40799de14c0a103dd80

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+
+   return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(id) {
+        console.log(id);
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarok!");
+        console.log(error.message);
+    });
+}
